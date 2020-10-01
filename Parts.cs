eebcb3afbb75e66a153c58f0126261903507a234
recs12 <slimane.rechdi@gmail.com>
2020-10-01 06:59:07 -0400

    Add project files.

---
+++ Parts.cs
@@ -0,0 +1,81 @@
+﻿
+using SolidEdgeAssembly;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cad
+{
+
+    public struct CadPart
+    {
+        public string JdeNumber;
+        public string Revision;
+        public string Filename;
+    }
+
+    public class Cache
+    {
+        public static CadPart ParseItem(string partCachePath)
+        {
+            SolidEdgeFileProperties.PropertySets propertySets = null;
+            SolidEdgeFileProperties.Properties properties = null;
+            SolidEdgeFileProperties.Property JDE = null;
+            SolidEdgeFileProperties.Property REVISION = null;
+            //SolidEdgeFileProperties.Property FILENAME = null;
+
+            try
+            {
+                // Create new instance of the PropertySets object
+                propertySets = new SolidEdgeFileProperties.PropertySets();
+
+                // Open a file
+                propertySets.Open(partCachePath, true);
+
+                // Get a reference to the SummaryInformation properties
+                properties = (SolidEdgeFileProperties.Properties)propertySets["ProjectInformation"];
+
+                //// Jde
+                JDE = (SolidEdgeFileProperties.Property)properties["Document Number"];
+
+                //// Revision
+                REVISION = (SolidEdgeFileProperties.Property)properties["Revision"];
+
+                //// filename
+                string filename = System.IO.Path.GetFileName(partCachePath);
+
+                // formatting for json extraction
+                var part = new CadPart
+                {
+                    JdeNumber = (string)JDE.Value,
+                    Revision = (string)REVISION.Value,
+                    Filename = filename
+                };
+
+                return part;
+
+                //Console.WriteLine(JDE.Value);
+                //Console.WriteLine(REVISION.Value);
+                //Console.WriteLine(filename);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                var part = new CadPart
+                {
+                    JdeNumber = null,
+                    Revision = null,
+                    Filename = null
+                };
+
+                return part;
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using shortid;
+using shortid.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SolidEdgeFileProperties;
+using Cad;
+
+namespace Extractor
+{
+    class Program
+    {
+        [STAThread]
+        [Obsolete]
+        static void Main()
+        {
+            try
+            {
+
+                //Refactoring
+                //
+                //GetFiles
+                //Structure
+                //args
+                //timing
+                //short ID for name file
+
+                string id = ShortId.Generate();
+                string path = @"C:\Users\Slimane\Desktop\dev\fasteners"+ id +".txt";
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+
+                        foreach (var itemPath in Directory.GetFiles(@"C:\Users\Slimane\Desktop\It's here\solidedge\Hardware\Washer"))
+                        {
+                            //string itemPath = @"C:\Users\Slimane\Desktop\It's here\solidedge\Hardware\Nuts\E_AB_0.138-32_SS.par";
+                            var screw = Cache.ParseItem(itemPath);
+                            if (Path.GetExtension(itemPath).ToLower() == ".par")
+                            {
+                                sw.WriteLine($"{0}:{current:{0}, revision:{1}, filename:{2}},", screw.JdeNumber, screw.Revision, screw.Filename);
+
+                            }
+                        }
+                    }
+                }
+
+
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
+
